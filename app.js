f0c2b61d4d26b1e5c2922af26b5019d23675ac42
Alok Tibrewala <alokpt92@gmail.com>
2019-04-25 03:28:08 -0400

    Initial app setup and creation of INDEX route

--- app.js
+++ app.js
@@ -2,3 +2,39 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose");
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/quickNotes");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/ MODEL CONFIG
+var quickNotesSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+
+var blog = mongoose.model("Blog", quickNotesSchema);
+
+//RESTFUL ROUTES
+
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+  blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("Cannot fetch blogs");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+  console.log("quickNotes server is running!");
+});
